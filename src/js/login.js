a875c43941a7fcce468a7b3ca47fbc37e91da644
wangqi <734180956@qq.com>
2016-12-08 15:24:38 +0800

    up

---
+++ src/js/login.js
@@ -0,0 +1,8 @@
+/*
+* @Author: Marte
+* @Date:   2016-12-06 17:47:49
+* @Last Modified by:   Marte
+* @Last Modified time: 2016-12-08 14:44:13
+*/
+
+'use strict';
---
+++ static/dist/js/register.js
@@ -0,0 +1,1 @@
+$("#content").click(function(){$(".content-wrapp").toggle()}),$(".content").click(function(){$("#area").html($(this).html()),$(".content-wrapp").hide()}),$(".fa-times-circle").click(function(){$(".phone-number").hide()});
